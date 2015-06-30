46681be8ea22c05bbd8d5d786f87c4b448f25ace
jon-a-nygaard <jon.arild@highsoft.com>
2015-06-30 19:55:38 +0200

    Minifyed output from Browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp'),
     browserify = require('browserify'),
-    source = require('vinyl-source-stream');
+    uglify = require('gulp-uglify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer');
 
+// @todo Minify before saving to destination.
 gulp.task('default', function () {
     return browserify('./src/app.js')
         .bundle()
         .pipe(source('app.min.js'))
+        .pipe(buffer())
+        .pipe(uglify())
         .pipe(gulp.dest('./dist/'));
 });
