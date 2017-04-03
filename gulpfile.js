4b90da2da40727627fee0e876d6608b98c81d24f
jon-a-nygaard <jon.arild@highsoft.com>
2017-04-03 20:34:52 +0200

    Updated build script to use npm scripts in stead of gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,6 @@ gulp.task('watch', function () {
     gulp.watch(['./src/*.js'], ['build']);
 });
 
-gulp.task('build', function () {
-    return browserify('./src/app.js')
-        .bundle()
-        .pipe(source('app.min.js'))
-        .pipe(buffer())
-        // .pipe(uglify())
-        .pipe(gulp.dest('./dist/'));
-});
-
 gulp.task('lint', function () {
     return gulp.src(['./src/*.js'])
         .pipe(jslint({}))
